ff082a5b315fe634ea6d41966ad5c8e15feaca3d
RITIK.J <ritikjha997147@gmail.com>
2026-05-14 14:59:40 +0530

    Set up SQLite database and reminders table
    Initialize SQLite database for reminders with a table.

--- bot/reminders/db.py
+++ bot/reminders/db.py
@@ -1,1 +1,20 @@
+import sqlite3
 
+
+conn = sqlite3.connect(
+    "bot/database/reminders.db",
+    check_same_thread=False
+)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reminders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chat_id INTEGER,
+    message TEXT,
+    remind_time REAL
+)
+""")
+
+conn.commit()
